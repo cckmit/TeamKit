d7e3f2bdde931cf1db79ed673a0566a67d83b1a9
fjay <fjayblue@126.com>
2017-12-14 15:14:36 +0800

    调整：调整常用ErrorCode

--- kit-core/src/main/java/org/team4u/kit/core/error/ErrorCode.java
+++ kit-core/src/main/java/org/team4u/kit/core/error/ErrorCode.java
@@ -8,14 +8,19 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class ErrorCode {
 
+    public static final ErrorCode SUCCESS = new ErrorCode("common", "0", "处理成功");
+
     public static final ErrorCode NOT_LOGIN = new ErrorCode("common", "not.login", "没有登录");
     public static final ErrorCode AUTH_ACCESS_DENIED = new ErrorCode("common", "auth.access.denied", "无权限访问");
+
+    public static final ErrorCode SYSTEM_ERROR = new ErrorCode("common", "system.error", "系统异常");
     public static final ErrorCode DATA_NOT_FOUND = new ErrorCode("common", "not.found", "%s数据没有找到");
+
     public static final ErrorCode PARAM_EMPTY = new ErrorCode("common", "param.empty", "%s参数为空");
     public static final ErrorCode PARAM_ERROR = new ErrorCode("common", "param.error", "非法参数：%s");
-    public static final ErrorCode PARAM_ERROR_TIME_START = new ErrorCode("common", "param.error.time.start", "结束时间不能小于开始时间");
-    public static final ErrorCode PARAM_ERROR_TIME_OVERFLOW = new ErrorCode("common", "param.error.time.overflow", "查询时间跨度不能超过 %s 天");
+
     private static final Map<String, ErrorCode> ERROR_CODES = new ConcurrentHashMap<String, ErrorCode>();
+
     private final String type;
     private final String code;
     private final String message;
