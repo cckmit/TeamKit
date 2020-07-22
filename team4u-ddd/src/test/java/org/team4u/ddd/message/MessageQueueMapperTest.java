a4c34f518691f65e5a23472da1fafe79012d552d
jay.wu <fjayblue@126.com>
2020-07-22 11:04:01 +0800

    新增：单元测试

---
+++ team4u-ddd/src/test/java/org/team4u/ddd/message/MessageQueueMapperTest.java
@@ -0,0 +1,72 @@
+package org.team4u.ddd.message;
+
+import cn.hutool.core.lang.func.VoidFunc1;
+import com.alibaba.fastjson.JSON;
+import org.junit.Assert;
+import org.junit.Test;
+import org.team4u.ddd.infrastructure.message.JsonMessageConverter;
+
+import java.io.IOException;
+
+public class MessageQueueMapperTest {
+
+    @Test
+    public void start() {
+        Queue q = new Queue();
+        Consumer c = new Consumer();
+        MessageQueueMapper mapper = new MessageQueueMapper(c, q, new JsonMessageConverter());
+        mapper.start();
+
+        String name = "fjay";
+        q.handler.callWithRuntimeException(JSON.toJSONString(new A().setName(name)));
+
+        Assert.assertEquals(name, c.getA().getName());
+    }
+
+    public static class A {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public A setName(String name) {
+            this.name = name;
+            return this;
+        }
+    }
+
+    public static class Consumer extends AbstractMessageConsumer<A> {
+
+        private A a;
+
+        @Override
+        protected void internalProcessMessage(A message) throws Throwable {
+            this.a = message;
+        }
+
+        public A getA() {
+            return a;
+        }
+    }
+
+    public static class Queue implements MessageQueue<String> {
+
+        private VoidFunc1<String> handler;
+
+        @Override
+        public void start() {
+
+        }
+
+        @Override
+        public void messageHandler(VoidFunc1<String> handler) {
+            this.handler = handler;
+        }
+
+        @Override
+        public void close() throws IOException {
+
+        }
+    }
+}
