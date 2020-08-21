0e197fd40e1aae304940f374f71c196877c2fd9d
jay.wu <fjayblue@126.com>
2020-08-21 16:19:09 +0800

    调整：新增数据库持久化配置

---
+++ team4u-config/src/main/java/org/team4u/config/infrastructure/persistence/SystemConfigMapper.java
@@ -0,0 +1,8 @@
+package org.team4u.config.infrastructure.persistence;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SystemConfigMapper extends BaseMapper<SystemConfig> {
+}
