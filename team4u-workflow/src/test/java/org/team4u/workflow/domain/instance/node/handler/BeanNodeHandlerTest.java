e33d8d4d6059a4fa618430be340844bfe5e08a0b
jay.wu <fjayblue@126.com>
2020-12-22 18:00:22 +0800

    新增：bean节点

--- team4u-workflow/src/test/java/org/team4u/workflow/domain/instance/node/handler/BeanNodeHandlerTest.java
+++ team4u-workflow/src/test/java/org/team4u/workflow/domain/instance/node/handler/BeanNodeHandlerTest.java
@@ -4,15 +4,17 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.team4u.workflow.TestUtil;
 import org.team4u.workflow.domain.definition.node.BeanNode;
+import org.team4u.workflow.domain.instance.exception.ProcessBeanHandlerNotExistException;
 
 import static org.team4u.workflow.TestUtil.TEST;
+import static org.team4u.workflow.TestUtil.TEST1;
 
 public class BeanNodeHandlerTest {
 
+    private final BeanNodeHandler handler = new TestBeanNodeHandler();
+
     @Test
     public void handle() {
-        BeanNodeHandler handler = new TestBeanNodeHandler();
-
         ProcessNodeHandlerContext context = TestUtil.contextBuilder()
                 .withInstance(TestUtil.newInstance())
                 .withNode(new BeanNode(TEST, TEST, TEST, null))
@@ -23,4 +25,19 @@ public class BeanNodeHandlerTest {
         Assert.assertEquals(TEST, context.ext(TEST));
     }
 
+    @Test
+    public void notExist() {
+        ProcessNodeHandlerContext context = TestUtil.contextBuilder()
+                .withInstance(TestUtil.newInstance())
+                .withNode(new BeanNode(TEST, TEST, TEST1, null))
+                .withAction(TestUtil.action(TEST))
+                .build();
+
+        try {
+            handler.handle(context);
+            Assert.fail();
+        } catch (ProcessBeanHandlerNotExistException e) {
+            Assert.assertEquals(TEST1, e.getMessage());
+        }
+    }
 }
