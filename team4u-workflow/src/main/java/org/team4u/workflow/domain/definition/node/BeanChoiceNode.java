60b8c561e61b666c04cf1e2853473f608ccd7c02
jay.wu <fjayblue@126.com>
2020-12-23 09:09:02 +0800

    新增：bean节点增加属性值

--- team4u-workflow/src/main/java/org/team4u/workflow/domain/definition/node/BeanChoiceNode.java
+++ team4u-workflow/src/main/java/org/team4u/workflow/domain/definition/node/BeanChoiceNode.java
@@ -1,5 +1,7 @@
 package org.team4u.workflow.domain.definition.node;
 
+import java.util.Map;
+
 /**
  * 基于bean的选择节点
  *
@@ -8,18 +10,34 @@ package org.team4u.workflow.domain.definition.node;
 public class BeanChoiceNode extends ChoiceNode {
 
     /**
-     * Bean名称
+     * bean名称
      */
     private final String beanName;
+    /**
+     * bean配置
+     */
+    private final Map<String, Object> beanConfig;
 
     public BeanChoiceNode(String nodeId,
                           String nodeName,
                           String beanName) {
+        this(nodeId, nodeName, beanName, null);
+    }
+
+    public BeanChoiceNode(String nodeId,
+                          String nodeName,
+                          String beanName,
+                          Map<String, Object> beanConfig) {
         super(nodeId, nodeName);
         this.beanName = beanName;
+        this.beanConfig = beanConfig;
     }
 
     public String getBeanName() {
         return beanName;
+    }
+
+    public Map<String, Object> getBeanConfig() {
+        return beanConfig;
     }
 }
--- team4u-workflow/src/main/java/org/team4u/workflow/domain/definition/node/BeanNode.java
+++ team4u-workflow/src/main/java/org/team4u/workflow/domain/definition/node/BeanNode.java
@@ -1,26 +1,44 @@
 package org.team4u.workflow.domain.definition.node;
 
+import java.util.Map;
+
 /**
  * bean节点
  *
  * @author jay.wu
  */
 public class BeanNode extends StaticNode {
-
     /**
-     * Bean名称
+     * bean名称
      */
     private final String beanName;
+    /**
+     * bean配置
+     */
+    private final Map<String, Object> beanConfig;
 
     public BeanNode(String nodeId,
                     String nodeName,
                     String beanName,
                     String nextNodeId) {
+        this(nodeId, nodeName, beanName, null, nextNodeId);
+    }
+
+    public BeanNode(String nodeId,
+                    String nodeName,
+                    String beanName,
+                    Map<String, Object> beanConfig,
+                    String nextNodeId) {
         super(nodeId, nodeName, nextNodeId);
         this.beanName = beanName;
+        this.beanConfig = beanConfig;
     }
 
     public String getBeanName() {
         return beanName;
+    }
+
+    public Map<String, Object> getBeanConfig() {
+        return beanConfig;
     }
 }
