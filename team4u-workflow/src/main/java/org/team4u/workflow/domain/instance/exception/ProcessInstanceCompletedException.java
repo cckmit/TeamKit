9da50388b27bc203c85e3a994023c337e660c100
jay.wu <fjayblue@126.com>
2020-12-30 17:54:53 +0800

    调整：完善异常

--- team4u-workflow/src/main/java/org/team4u/workflow/domain/instance/exception/ProcessInstanceCompletedException.java
+++ team4u-workflow/src/main/java/org/team4u/workflow/domain/instance/exception/ProcessInstanceCompletedException.java
@@ -1,13 +1,13 @@
 package org.team4u.workflow.domain.instance.exception;
 
-import org.team4u.workflow.domain.ProcessBusinessException;
+import org.team4u.workflow.domain.ProcessSystemException;
 
 /**
  * 流程已结束异常
  *
  * @author jay.wu
  */
-public class ProcessInstanceCompletedException extends ProcessBusinessException {
+public class ProcessInstanceCompletedException extends ProcessSystemException {
 
     public ProcessInstanceCompletedException(String message) {
         super(message);
