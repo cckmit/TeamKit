aa6bbcd8ddc0cad456c6c5f12b9eb73ade64b327
jay.wu <fjayblue@126.com>
2021-02-05 11:03:44 +0800

    新增：人工控制的长时线程

---
+++ team4u-base/src/main/java/org/team4u/base/lang/ManuallyLongTimeThread.java
@@ -0,0 +1,86 @@
+package org.team4u.base.lang;
+
+import cn.hutool.core.thread.ThreadUtil;
+import cn.hutool.log.Log;
+import cn.hutool.log.LogFactory;
+
+/**
+ * 手工控制的长时线程
+ *
+ * @author jay.wu
+ */
+public abstract class ManuallyLongTimeThread extends LongTimeThread {
+
+    private static final Log log = LogFactory.get();
+
+    private int runCount = 0;
+
+    public ManuallyLongTimeThread() {
+    }
+
+    public ManuallyLongTimeThread(Runnable target) {
+        super(target);
+    }
+
+    public ManuallyLongTimeThread(ThreadGroup group, Runnable target) {
+        super(group, target);
+    }
+
+    public ManuallyLongTimeThread(String name) {
+        super(name);
+    }
+
+    public ManuallyLongTimeThread(ThreadGroup group, String name) {
+        super(group, name);
+    }
+
+    public ManuallyLongTimeThread(Runnable target, String name) {
+        super(target, name);
+    }
+
+    public ManuallyLongTimeThread(ThreadGroup group, Runnable target, String name) {
+        super(group, target, name);
+    }
+
+    public ManuallyLongTimeThread(ThreadGroup group, Runnable target, String name, long stackSize) {
+        super(group, target, name, stackSize);
+    }
+
+    @Override
+    public void run() {
+        while (!isClosed()) {
+            if (!isEnabled() || runCount >= maxRunCount()) {
+                ThreadUtil.safeSleep(runIntervalMillis());
+                continue;
+            }
+
+            ++runCount;
+
+            try {
+                onRun();
+            } catch (Exception e) {
+                log.error(e, e.getMessage());
+            }
+
+            ThreadUtil.safeSleep(runIntervalMillis());
+        }
+    }
+
+    public int getRunCount() {
+        return runCount;
+    }
+
+    /**
+     * 获取是否开启任务
+     *
+     * @return 是否开启
+     */
+    protected abstract boolean isEnabled();
+
+    /**
+     * 获取最大运行次数
+     *
+     * @return 最大运行次数
+     */
+    protected abstract int maxRunCount();
+}
---
+++ team4u-base/src/test/java/org/team4u/base/lang/ManuallyLongTimeThreadTest.java
@@ -0,0 +1,54 @@
+package org.team4u.base.lang;
+
+import cn.hutool.core.thread.ThreadUtil;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ManuallyLongTimeThreadTest {
+
+    @Test
+    public void notEnabled() {
+        TestThread t = new TestThread(false, 1);
+        t.run();
+        Assert.assertEquals(0, t.getRunCount());
+    }
+
+    @Test
+    public void runTwice() {
+        TestThread t = new TestThread(true, 2);
+        t.start();
+        ThreadUtil.sleep(100);
+        Assert.assertEquals(2, t.getRunCount());
+    }
+
+    private static class TestThread extends ManuallyLongTimeThread {
+
+        private final boolean isEnabled;
+        private final int maxRunCount;
+
+        private TestThread(boolean isEnabled, int maxRunCount) {
+            this.isEnabled = isEnabled;
+            this.maxRunCount = maxRunCount;
+        }
+
+        @Override
+        protected void onRun() {
+
+        }
+
+        @Override
+        protected Number runIntervalMillis() {
+            return 1;
+        }
+
+        @Override
+        protected boolean isEnabled() {
+            return isEnabled;
+        }
+
+        @Override
+        protected int maxRunCount() {
+            return maxRunCount;
+        }
+    }
+}
