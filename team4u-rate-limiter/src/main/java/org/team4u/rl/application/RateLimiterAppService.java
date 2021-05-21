f2b7b8739dab10cfb2a9a21be4bb496ea8835eb8
jay.wu <fjayblue@126.com>
2021-05-21 11:46:03 +0800

    feat(limiter):新增简单限流器功能

---
+++ team4u-rate-limiter/src/main/java/org/team4u/rl/application/RateLimiterAppService.java
@@ -0,0 +1,28 @@
+package org.team4u.rl.application;
+
+import org.team4u.rl.domain.RateLimiterService;
+
+/**
+ * 限流应用服务
+ *
+ * @author Jay Wu
+ */
+public class RateLimiterAppService {
+
+    private final RateLimiterService[] limiterServices;
+
+    public RateLimiterAppService(RateLimiterService... limiterServices) {
+        this.limiterServices = limiterServices;
+    }
+
+    public boolean tryAcquire(String type, String key) {
+        // 任意一个限流管理器不允许访问，则返回false
+        for (RateLimiterService rateLimiter : limiterServices) {
+            if (!rateLimiter.tryAcquire(type, key)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
