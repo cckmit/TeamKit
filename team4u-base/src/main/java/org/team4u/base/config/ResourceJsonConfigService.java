f7135fe7d85c033c15457cd025c17c4add90a5a6
jay.wu <fjayblue@126.com>
2021-05-21 15:58:22 +0800

    feat(limiter):新增简单限流器功能

--- team4u-base/src/main/java/org/team4u/base/config/ResourceJsonConfigService.java
+++ team4u-base/src/main/java/org/team4u/base/config/ResourceJsonConfigService.java
@@ -1,24 +1,23 @@
 package org.team4u.base.config;
 
 import cn.hutool.core.convert.Convert;
-import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.io.resource.ResourceUtil;
 import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.log.Log;
-import org.team4u.base.util.PathUtil;
 
 /**
  * 本地Json文件配置服务
  *
  * @author jay.wu
  */
-public class LocalJsonConfigService extends ResourceJsonConfigService {
+public class ResourceJsonConfigService implements ConfigService {
 
     private final Log log = Log.get();
 
     @Override
     public String get(String key) {
         try {
-            return FileUtil.readUtf8String(PathUtil.runningPath(key + ".json"));
+            return ResourceUtil.readUtf8Str(key + ".json");
         } catch (Exception e) {
             log.warn(e.getMessage());
             return null;
