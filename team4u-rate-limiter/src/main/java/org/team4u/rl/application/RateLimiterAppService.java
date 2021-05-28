9bf97fab1161cf433309e5b2fa9b2a4067957ec5
jay.wu <fjayblue@126.com>
2021-05-28 11:37:24 +0800

    feat(limiter):完善限流服务备注

--- team4u-rate-limiter/src/main/java/org/team4u/rl/application/RateLimiterAppService.java
+++ team4u-rate-limiter/src/main/java/org/team4u/rl/application/RateLimiterAppService.java
@@ -17,11 +17,25 @@ public class RateLimiterAppService {
         this.limiterServices = limiterServices;
     }
 
+    /**
+     * 根据类型尝试是否允许访问
+     *
+     * @param type 类型
+     * @param key  键值
+     * @return true为可以访问，false为拒绝访问
+     */
     public boolean tryAcquire(String type, String key) {
         return Arrays.stream(limiterServices)
                 .allMatch(it -> it.tryAcquire(type, key));
     }
 
+    /**
+     * 获取尝试访问次数
+     *
+     * @param type 类型
+     * @param key  键值
+     * @return 成功访问次数
+     */
     public long tryAcquiredCount(String type, String key) {
         return Arrays.stream(limiterServices)
                 .map(it -> it.tryAcquiredCount(type, key))
@@ -29,6 +43,13 @@ public class RateLimiterAppService {
                 .orElse(0L);
     }
 
+    /**
+     * 是否可以访问
+     *
+     * @param type 类型
+     * @param key  键值
+     * @return 是否可以访问
+     */
     public boolean canAcquire(String type, String key) {
         return Arrays.stream(limiterServices)
                 .allMatch(it -> it.canAcquire(type, key));
