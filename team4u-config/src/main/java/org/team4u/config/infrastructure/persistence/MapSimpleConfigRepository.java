c7fcb15fc7a55e525a1f605e50d7a43b98576683
jay.wu <fjayblue@126.com>
2021-06-28 09:41:35 +0800

    调整：完善MapSimpleConfigRepository的类型获取

--- team4u-config/src/main/java/org/team4u/config/infrastructure/persistence/MapSimpleConfigRepository.java
+++ team4u-config/src/main/java/org/team4u/config/infrastructure/persistence/MapSimpleConfigRepository.java
@@ -1,6 +1,6 @@
 package org.team4u.config.infrastructure.persistence;
 
-import cn.hutool.core.util.StrUtil;
+import cn.hutool.core.lang.Pair;
 import org.team4u.config.domain.SimpleConfig;
 import org.team4u.config.domain.SimpleConfigId;
 import org.team4u.config.domain.SimpleConfigRepository;
@@ -24,19 +24,10 @@ public class MapSimpleConfigRepository implements SimpleConfigRepository {
                 .entrySet()
                 .stream()
                 .map(it -> {
-                    String[] typeAndKey = StrUtil.splitToArray(it.getKey().toString(), ".");
-                    String type;
-                    String key;
-                    if (typeAndKey.length == 1) {
-                        type = "";
-                        key = typeAndKey[0];
-                    } else {
-                        type = typeAndKey[0];
-                        key = typeAndKey[1];
-                    }
+                    Pair<String, String> typeAndKey = typeAndKey(it.getKey().toString());
 
                     return new SimpleConfig(
-                            new SimpleConfigId(type, key),
+                            new SimpleConfigId(typeAndKey.getKey(), typeAndKey.getValue()),
                             it.getValue().toString(),
                             null,
                             0,
@@ -46,6 +37,17 @@ public class MapSimpleConfigRepository implements SimpleConfigRepository {
                     );
                 })
                 .collect(Collectors.toList());
+    }
+
+    protected Pair<String, String> typeAndKey(String originalKey) {
+        int lastDotIndex = originalKey.lastIndexOf('.');
+        if (lastDotIndex <= 0) {
+            return new Pair<>("", originalKey);
+        }
+
+        String type = originalKey.substring(0, lastDotIndex);
+        String key = originalKey.substring(lastDotIndex + 1);
+        return new Pair<>(type, key);
     }
 
     public Map<?, ?> getSource() {
---
+++ team4u-config/src/test/java/org/team4u/config/infrastructure/persistence/MapSimpleConfigRepositoryTest.java
@@ -0,0 +1,28 @@
+package org.team4u.config.infrastructure.persistence;
+
+import cn.hutool.core.lang.Pair;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MapSimpleConfigRepositoryTest {
+
+    @Test
+    public void typeAndKey() {
+        MapSimpleConfigRepository r = new MapSimpleConfigRepository(null);
+        Pair<String, String> tk = r.typeAndKey("x.y.z");
+        Assert.assertEquals("x.y", tk.getKey());
+        Assert.assertEquals("z", tk.getValue());
+
+        tk = r.typeAndKey("x.y");
+        Assert.assertEquals("x", tk.getKey());
+        Assert.assertEquals("y", tk.getValue());
+
+        tk = r.typeAndKey(".x");
+        Assert.assertEquals("", tk.getKey());
+        Assert.assertEquals(".x", tk.getValue());
+
+        tk = r.typeAndKey("x");
+        Assert.assertEquals("", tk.getKey());
+        Assert.assertEquals("x", tk.getValue());
+    }
+}
