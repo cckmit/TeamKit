bcc75b14400006e1e1a25950fe57ab11f5c49490
jay.wu <fjayblue@126.com>
2021-07-26 18:35:55 +0800

    refactor(config): 完善bean配置

--- team4u-test/src/main/java/org/team4u/test/spring/BaseTestBeanConfig.java
+++ team4u-test/src/main/java/org/team4u/test/spring/BaseTestBeanConfig.java
@@ -1,19 +1,15 @@
 package org.team4u.test.spring;
 
 import cn.hutool.extra.spring.SpringUtil;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * 基本测试bean配置
  *
  * @author jay.wu
  */
+@Import(SpringUtil.class)
 @Configuration
 public class BaseTestBeanConfig {
-
-    @Bean
-    public SpringUtil stringUtil() {
-        return new SpringUtil();
-    }
 }
