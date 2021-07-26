cf4d2cf95fe47b12be0bb24590371c2cb0ea3837
jay.wu <fjayblue@126.com>
2021-07-26 15:27:22 +0800

    refactor(func): 重构无入参函数

--- team4u-base/src/main/java/org/team4u/base/lang/CacheableFunc0.java
+++ team4u-base/src/main/java/org/team4u/base/lang/CacheableFunc0.java
@@ -1,9 +1,8 @@
 package org.team4u.base.lang;
 
+import cn.hutool.cache.Cache;
+import cn.hutool.cache.CacheUtil;
 import cn.hutool.core.lang.func.Func0;
-import org.team4u.base.error.NestedException;
-
-import java.util.Arrays;
 
 /**
  * 可缓存结果的无参数函数
@@ -13,31 +12,22 @@ import java.util.Arrays;
  */
 public abstract class CacheableFunc0<R> implements Func0<R> {
 
-    /**
-     * 未初始化结果值
-     */
-    private static final Object NOT_INIT = new Object();
-
-    private final Class<?>[] cacheForExceptionClasses;
-
-    /**
-     * 缓存的执行结果
-     */
-    private Object result = NOT_INIT;
-    /**
-     * 缓存的执行异常
-     */
-    private Exception e;
+    private final Delegate delegate;
 
     public CacheableFunc0() {
-        this(null);
+        this((Class<?>[]) null);
     }
 
-    /**
-     * @param cacheForExceptionClasses 需要缓存的异常类型，为null时不缓存
-     */
     public CacheableFunc0(Class<?>[] cacheForExceptionClasses) {
-        this.cacheForExceptionClasses = cacheForExceptionClasses;
+        this(CacheUtil.newLRUCache(1), cacheForExceptionClasses);
+    }
+
+    public CacheableFunc0(Cache<Class<?>, R> cache) {
+        this(cache, null);
+    }
+
+    public CacheableFunc0(Cache<Class<?>, R> cache, Class<?>[] cacheForExceptionClasses) {
+        delegate = new Delegate(cache, cacheForExceptionClasses);
     }
 
     /**
@@ -46,43 +36,13 @@ public abstract class CacheableFunc0<R> implements Func0<R> {
      * @return 函数执行结果，初次执行后将缓存结果，后续再次执行时直接返回缓存结果
      * @throws Exception 自定义异常
      */
-    @SuppressWarnings("unchecked")
     public R callWithCache() throws Exception {
-        if (e != null) {
-            throw e;
-        }
-
-        if (result != NOT_INIT) {
-            return (R) result;
-        }
-
-        synchronized (this) {
-            if (result != NOT_INIT) {
-                return (R) result;
-            }
-
-            try {
-                result = call();
-                return (R) result;
-            } catch (Exception e) {
-                if (canCacheForError(e)) {
-                    this.e = e;
-                }
-
-                throw e;
-            }
-        }
+        return delegate.callWithCache(this.getClass());
     }
 
     @Override
     public R callWithRuntimeException() {
-        try {
-            return call();
-        } catch (RuntimeException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new NestedException(e);
-        }
+        return delegate.callWithRuntimeException(this.getClass());
     }
 
     /**
@@ -91,35 +51,26 @@ public abstract class CacheableFunc0<R> implements Func0<R> {
      * @return 函数执行结果，初次执行后将缓存结果，后续再次执行时直接返回缓存结果
      */
     public R callWithCacheAndRuntimeException() {
-        try {
-            return callWithCache();
-        } catch (RuntimeException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new NestedException(e);
-        }
+        return delegate.callWithCacheAndRuntimeException(this.getClass());
     }
 
-    /**
-     * 重置缓存结果
-     */
     public void reset() {
-        result = NOT_INIT;
-        e = null;
+        delegate.reset();
     }
 
-    /**
-     * 判断是否可缓存异常
-     *
-     * @param e 异常对象
-     * @return true：可缓存，false：不缓存
-     */
-    private boolean canCacheForError(Exception e) {
-        if (cacheForExceptionClasses == null) {
-            return false;
+    private class Delegate extends CacheableFunc1<Class<?>, R> {
+
+        public Delegate(Cache<Class<?>, R> cache) {
+            super(cache);
         }
 
-        return Arrays.stream(cacheForExceptionClasses)
-                .anyMatch(it -> it.isAssignableFrom(e.getClass()));
+        public Delegate(Cache<Class<?>, R> cache, Class<?>[] cacheForExceptionClasses) {
+            super(cache, cacheForExceptionClasses);
+        }
+
+        @Override
+        public R call(Class<?> parameter) throws Exception {
+            return CacheableFunc0.this.call();
+        }
     }
 }
