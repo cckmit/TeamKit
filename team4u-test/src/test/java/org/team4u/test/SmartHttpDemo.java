3ed5feb3250c5af03078e87fce5042e19b20499d
jay.wu <fjayblue@126.com>
2021-07-27 14:24:32 +0800

    build(pom): 升级公共包

--- team4u-test/src/test/java/org/team4u/test/SmartHttpDemo.java
+++ team4u-test/src/test/java/org/team4u/test/SmartHttpDemo.java
@@ -1,120 +1,30 @@
 package org.team4u.test;
 
-import org.smartboot.http.server.*;
-import org.smartboot.http.server.handle.HttpRouteHandle;
-import org.smartboot.http.server.handle.WebSocketDefaultHandle;
-import org.smartboot.http.server.handle.WebSocketRouteHandle;
+import cn.hutool.json.JSONUtil;
+import org.smartboot.http.server.HttpBootstrap;
+import org.smartboot.http.server.HttpRequest;
+import org.smartboot.http.server.HttpResponse;
+import org.smartboot.http.server.HttpServerHandler;
+import org.smartboot.http.server.handler.HttpRouteHandler;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Collection;
+import java.nio.charset.StandardCharsets;
 
 public class SmartHttpDemo {
 
     public static void main(String[] args) {
-        System.setProperty("smartHttp.server.alias", "SANDAO base on ");
-
-        HttpRouteHandle routeHandle = new HttpRouteHandle();
-        routeHandle.route("/", new HttpServerHandle() {
-            final byte[] body = ("<html>" +
-                    "<head><title>smart-http demo</title></head>" +
-                    "<body>" +
-                    "GET 表单提交<form action='/get' method='get'><input type='text' name='text'/><input type='submit'/></form></br>" +
-                    "POST 表单提交<form action='/post' method='post'><input type='text' name='text'/><input type='submit'/></form></br>" +
-                    "文件上传<form action='/upload' method='post' enctype='multipart/form-data'><input type='file' name='text'/><input type='submit'/></form></br>" +
-                    "</body></html>").getBytes();
-
+        HttpRouteHandler routeHandler = new HttpRouteHandler();
+        routeHandler.route("/", new HttpServerHandler() {
             @Override
-            public void doHandle(HttpRequest request, HttpResponse response) throws IOException {
-
-                response.setContentLength(body.length);
-                response.getOutputStream().write(body);
+            public void handle(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException {
+                httpResponse.write("1234".getBytes(StandardCharsets.UTF_8));
             }
-        }).route("/get", new HttpServerHandle() {
+        }).route("/a", new HttpServerHandler() {
             @Override
-            public void doHandle(HttpRequest request, HttpResponse response) throws IOException {
-                response.write(("收到Get参数text=" + request.getParameter("text")).getBytes());
-            }
-        }).route("/post", new HttpServerHandle() {
-            @Override
-            public void doHandle(HttpRequest request, HttpResponse response) throws IOException {
-                response.write(("收到Post参数text=" + request.getParameter("text")).getBytes());
-            }
-        }).route("/upload", new HttpServerHandle() {
-            @Override
-            public void doHandle(HttpRequest request, HttpResponse response) throws IOException {
-                InputStream in = request.getInputStream();
-                byte[] buffer = new byte[1024];
-                int len;
-                while ((len = in.read(buffer)) != -1) {
-                    response.getOutputStream().write(buffer, 0, len);
-                }
-                in.close();
-            }
-        }).route("/post_json", new HttpServerHandle() {
-            @Override
-            public void doHandle(HttpRequest request, HttpResponse response) throws IOException {
-                InputStream in = request.getInputStream();
-                byte[] buffer = new byte[1024];
-                int len;
-                System.out.println(request.getContentType());
-                while ((len = in.read(buffer)) != -1) {
-                    response.getOutputStream().write(buffer, 0, len);
-                }
-                in.close();
-            }
-        }).route("/plaintext", new HttpServerHandle() {
-            final byte[] body = "Hello World!".getBytes();
-
-            @Override
-            public void doHandle(HttpRequest request, HttpResponse response) throws IOException {
-                response.setContentLength(body.length);
-                response.setContentType("text/plain; charset=UTF-8");
-                response.write(body);
-            }
-        }).route("/head", new HttpServerHandle() {
-            @Override
-            public void doHandle(HttpRequest request, HttpResponse response) throws IOException {
-                response.addHeader("a", "b");
-                response.addHeader("a", "c");
-                Collection<String> headNames = request.getHeaderNames();
-                for (String headerName : headNames) {
-                    response.write((headerName + ": " + request.getHeaders(headerName) + "</br>").getBytes());
-                }
+            public void handle(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException {
+                httpResponse.write(JSONUtil.toJsonStr(httpRequest.getParameters()).getBytes(StandardCharsets.UTF_8));
             }
         });
-
-
-        HttpBootstrap bootstrap = new HttpBootstrap();
-        //配置HTTP消息处理管道
-        bootstrap.pipeline().next(routeHandle);
-
-        WebSocketRouteHandle wsRouteHandle = new WebSocketRouteHandle();
-        wsRouteHandle.route("/ws", new WebSocketDefaultHandle() {
-            @Override
-            public void onHandShake(WebSocketRequest request, WebSocketResponse webSocketResponse) {
-                System.out.println("收到握手消息");
-            }
-
-            @Override
-            public void handleTextMessage(WebSocketRequest request, WebSocketResponse response, String data) {
-                System.out.println("收到请求消息:" + data);
-                response.sendTextMessage("服务端收到响应:" + data);
-            }
-
-            @Override
-            public void onClose(WebSocketRequest request, WebSocketResponse response) {
-                super.onClose(request, response);
-            }
-
-            @Override
-            public void handleBinaryMessage(WebSocketRequest request, WebSocketResponse response, byte[] data) {
-                response.sendBinaryMessage(data);
-            }
-        });
-        bootstrap.wsPipeline().next(wsRouteHandle);
-
-        //设定服务器配置并启动
-        bootstrap.setPort(8080).start();
+        new HttpBootstrap().pipeline(routeHandler).setPort(8080).start();
     }
 }
