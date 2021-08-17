2fb022788db0f08f580b481ad385b0a1c4ff1d28
jay.wu <fjayblue@126.com>
2021-08-17 17:52:53 +0800

    feat(bean): 新增异常NoSuchBeanDefinitionException

--- team4u-base/src/main/java/org/team4u/base/bean/provider/NoSuchBeanDefinitionException.java
+++ team4u-base/src/main/java/org/team4u/base/bean/provider/NoSuchBeanDefinitionException.java
@@ -2,6 +2,11 @@ package org.team4u.base.bean.provider;
 
 import org.team4u.base.error.SystemDataNotExistException;
 
+/**
+ * 无法找到bean异常
+ *
+ * @author jay.wu
+ */
 public class NoSuchBeanDefinitionException extends SystemDataNotExistException {
 
     public NoSuchBeanDefinitionException() {
