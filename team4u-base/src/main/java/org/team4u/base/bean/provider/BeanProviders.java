0ce668b6039537c12a135bf1564e208fcdc906ba
jay.wu <fjayblue@126.com>
2021-08-20 11:12:57 +0800

    doc(bean): 完善注释

--- team4u-base/src/main/java/org/team4u/base/bean/provider/BeanProviders.java
+++ team4u-base/src/main/java/org/team4u/base/bean/provider/BeanProviders.java
@@ -3,6 +3,7 @@ package org.team4u.base.bean.provider;
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.lang.Singleton;
 import lombok.extern.slf4j.Slf4j;
+import org.team4u.base.bean.event.BeanInitializedEvent;
 import org.team4u.base.lang.IdObjectService;
 import org.team4u.base.log.LogMessage;
 
@@ -13,8 +14,14 @@ import java.util.Objects;
 
 /**
  * bean提供者服务
+ * <p>
+ * 设计目的：
+ * - 很多通用服务需要一套完整的扩展服务，配合IdObjectService可以实现一部分需求，但无法方便地与spring联动
+ * - 该类提供一套简单通用的bean管理功能供通用服务扩展，在与特定框架解耦（如Spring）的同时，通过监听BeanInitializedEvent实现特定框架的回调注册
  *
  * @author jay.wu
+ * @see IdObjectService
+ * @see BeanInitializedEvent
  */
 @Slf4j
 public class BeanProviders extends IdObjectService<String, BeanProvider> {
@@ -68,6 +75,8 @@ public class BeanProviders extends IdObjectService<String, BeanProvider> {
 
     /**
      * 获取指定类型对应的所有Bean，包括子类
+     * <p>
+     * 如果存在同名n，将返回优先级高的bean
      *
      * @param <T>  Bean类型
      * @param type 类、接口，null表示获取所有bean
@@ -86,9 +95,12 @@ public class BeanProviders extends IdObjectService<String, BeanProvider> {
 
     /**
      * 注册Bean
+     * <p>
+     * 注册成功后将发布BeanInitializedEvent事件
      *
      * @param <T>  Bean类型
      * @param bean bean
+     * @see BeanInitializedEvent
      */
     public <T> boolean registerBean(String beanName, T bean) {
         LogMessage lm = LogMessage.create(this.getClass().getSimpleName(), "registerBean")
