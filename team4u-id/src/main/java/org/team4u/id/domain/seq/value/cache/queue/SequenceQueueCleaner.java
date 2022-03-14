c6ac5fc71b87890554c207f8bc32f6761f77276a
jay.wu <fjayblue@126.com>
2022-03-14 17:02:37 +0800

    refactor(seq): 优化代码

--- team4u-id/src/main/java/org/team4u/id/domain/seq/value/cache/queue/SequenceQueueCleaner.java
+++ team4u-id/src/main/java/org/team4u/id/domain/seq/value/cache/queue/SequenceQueueCleaner.java
@@ -67,7 +67,6 @@ public class SequenceQueueCleaner extends LongTimeThread {
         return System.currentTimeMillis() - queue.getStatus().getOccurredOn() > config.getExpiredWhenQueueExhaustedMillis();
     }
 
-
     @Override
     protected Number runIntervalMillis() {
         return config.getQueueCleanerRunIntervalMillis();
