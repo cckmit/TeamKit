670c595926fee6c0c86fa87ef4cdfab9006dd8b1
jay.wu <fjayblue@126.com>
2022-03-15 14:42:44 +0800

    feat(selector): 优化权重/概率选择器配置

--- team4u-base/src/main/java/org/team4u/base/util/MapExtUtil.java
+++ team4u-base/src/main/java/org/team4u/base/util/MapExtUtil.java
@@ -5,14 +5,29 @@ import cn.hutool.core.convert.Convert;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+/**
+ * Map扩展工具类
+ *
+ * @author jay.wu
+ */
 public class MapExtUtil {
 
-    public static <K, V> Map<K, V> convert(Map<?, ?> source, Class<K> keyClass, Class<V> valueClass) {
+    /**
+     * 将来源Map转换为目标类型的Map
+     *
+     * @param source           来源Map
+     * @param targetKeyClass   目标Map的key类型
+     * @param targetValueClass 目标Map的value类型
+     * @param <K>              目标Map的key类型
+     * @param <V>目标Map的value类型
+     * @return 目标Map
+     */
+    public static <K, V> Map<K, V> convert(Map<?, ?> source, Class<K> targetKeyClass, Class<V> targetValueClass) {
         return source.entrySet()
                 .stream()
                 .collect(Collectors.toMap(
-                        it -> Convert.convert(keyClass, it.getKey()),
-                        it -> Convert.convert(valueClass, it.getValue())
+                        it -> Convert.convert(targetKeyClass, it.getKey()),
+                        it -> Convert.convert(targetValueClass, it.getValue())
                 ));
     }
 }
