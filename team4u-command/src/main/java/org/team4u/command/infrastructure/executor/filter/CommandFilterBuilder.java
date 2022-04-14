5ac271004cb9308f15d7906bb678e498162ca6fd
jay.wu <fjayblue@126.com>
2022-04-14 09:55:09 +0800

    feat(command): 新增根据命令过滤器构建顺序过滤器

--- team4u-command/src/main/java/org/team4u/command/infrastructure/executor/filter/CommandFilterBuilder.java
+++ team4u-command/src/main/java/org/team4u/command/infrastructure/executor/filter/CommandFilterBuilder.java
@@ -1,5 +1,6 @@
 package org.team4u.command.infrastructure.executor.filter;
 
+import org.team4u.base.filter.v2.Filter;
 import org.team4u.command.domain.executor.handler.CommandHandler;
 
 /**
@@ -18,6 +19,19 @@ public class CommandFilterBuilder {
     public static CommandFilter filter(CommandHandler commandHandler) {
         return context -> {
             commandHandler.handle(context);
+            return true;
+        };
+    }
+
+    /**
+     * 根据命令过滤器构建顺序过滤器
+     *
+     * @param filter 命令过滤器
+     * @return 顺序过滤器
+     */
+    public static CommandFilter filter(Filter<CommandHandler.Context> filter) {
+        return context -> {
+            filter.doFilter(context);
             return true;
         };
     }
